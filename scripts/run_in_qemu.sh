fe8530aedfb8632cb2f51f2d1a741197b40d89d6
Nikolai Ponomarev <wowasterdev@gmail.com>
2023-07-22 14:46:33 +0300

    Move building logic for RISC-V in separate script

---
+++ scripts/run_in_qemu.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -euxo pipefail
+
+echo "Installing Debian packages"
+apt-get update
+apt-get install -y build-essential qt5-qmake qtbase5-dev qtmultimedia5-dev libqt5svg5-dev qtscript5-dev rsync libusb-1.0-0-dev python3-dev libqt5serialport5-dev libudev-dev libqt5svg5-dev qtwayland5 qtbase5-private-dev qttools5-dev-tools ccache
+
+echo "Checking available tools"
+uname -a
+rsync --version
+qmake --version && qmake -query
+python3 --version
+which g++
+g++ --version
+ccache --version
+
+qmakeConfig="CONFIG+=release CONFIG+=trik_nopython CONFIG+=tests CONFIG+=noPch CONFIG+=ccache CONFIG+=sanitizer CONFIG+=sanitize_undefined"
+
+echo "Configuring with QMake"
+qmake $qmakeConfig .
+
+echo "Running qmake_all"
+make -j $(nproc) qmake_all
+
+echo "Making project"
+make -j $(nproc) all
+
+echo "Running unit tests"
+make -k check TESTARGS="-platform offscreen"
