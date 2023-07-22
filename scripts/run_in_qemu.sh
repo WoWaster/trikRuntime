a88f62d5e369fda996d3174f7b330970559c0263
Nikolai Ponomarev <wowasterdev@gmail.com>
2023-07-22 16:02:31 +0300

    Try to minimize dependencies

--- scripts/run_in_qemu.sh
+++ scripts/run_in_qemu.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 
 echo "Installing Debian packages"
 apt-get update
-apt-get install -y build-essential qt5-qmake qtbase5-dev qtmultimedia5-dev libqt5svg5-dev qtscript5-dev rsync libusb-1.0-0-dev python3-dev libqt5serialport5-dev libudev-dev libqt5svg5-dev qtwayland5 qtbase5-private-dev qttools5-dev-tools ccache
+apt-get install --yes --no-install-recommends build-essential qt5-qmake qtbase5-dev qtmultimedia5-dev libqt5svg5-dev qtscript5-dev rsync libusb-1.0-0-dev python3-dev libqt5serialport5-dev libudev-dev libqt5svg5-dev qtwayland5 qtbase5-private-dev qttools5-dev-tools ccache
 
 echo "Checking available tools"
 uname -a
